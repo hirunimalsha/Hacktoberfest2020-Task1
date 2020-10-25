dd9a82040fbc17c7779d47070f7def56406bacb2
Hiruni Malsha <45029980+hirunimalsha@users.noreply.github.com>
2020-10-26 03:24:29 +0530

    Update Program.c

--- Program.c
+++ Program.c
@@ -3,9 +3,10 @@
 int main()
 {
     char name,country,skill;
+    int age;
     printf("Welcome to Hacktoberfest 2020\n");
     printf("Enter your name : ");//Get User name
-    scanf("%d",&name);
+    scanf("%s",&name);
     printf("Enter your Country of Residance : ");//Get user country
     scanf("%s",&country);
     printf("Enter your Age : ");//Get user Age
